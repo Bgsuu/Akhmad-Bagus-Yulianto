d1a8bd48a0680043b017b91689eb7c313c5e6fd2
Bgsuu <48554997+Bgsuu@users.noreply.github.com>
2019-03-14 19:32:39 +0700

    Add files via upload

---
+++ bagus.php
@@ -0,0 +1,10 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?> 
+   <?php echo '<p>Nama: Akhmad Bagus Yulianto</p>'; ?> 
+    <?php echo '<p>NIM: 16.01.53.0040</p>'; ?> 
+ </body>
+</html>
